52a3140eaeb618ac41e735b57ea20adf5c686069
hitnik <hitnik@gmail.com>
2021-08-17 20:16:14 +0300

    create db_init, get_db, close_db functions

---
+++ app/db.py
@@ -0,0 +1,23 @@
+import sqlite3
+import os 
+
+BASEDIR = os.path.dirname(__file__)
+
+DB_PATH = os.path.join(BASEDIR, 'db.sqlite3')
+
+def init_db():
+    db = sqlite3.connect(DB_PATH, detect_types=sqlite3.PARSE_DECLTYPES)
+    with open(os.path.join(BASEDIR, 'schema.sql'), 'rb') as file:
+        db.executescript(file.read().decode('utf8'))
+
+def get_db():
+    db = sqlite3.connect(DB_PATH, detect_types=sqlite3.PARSE_DECLTYPES)
+    return db
+
+def close_db(db):
+    if db is not None:
+        db.close()
+
+if __name__ == '__main__':
+    init_db()
+    
