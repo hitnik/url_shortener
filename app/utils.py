ceea12a84331dedde46ecf8c725409ff2ab01078
hitnik <hitnik@gmail.com>
2021-08-18 21:34:37 +0300

    code refactor

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,33 @@
-from unittest.main import main
+import argparse
+import textwrap
 from db import ( short_url_exist, get_short_url,
                     get_long_url_from_db, init_db
                 )
+
+
+def parser():
+    parser = argparse.ArgumentParser(
+                                        description="URL shortener app.",
+                                        formatter_class=argparse.RawTextHelpFormatter
+                                    )
+    parser.add_argument('url', metavar='URL', type=str,
+                            help=textwrap.dedent("""
+                                Provide URL here.\r
+                                If you want to shorten URL, """ +
+                                """provide it with [--generate] argument.\r
+                                If you want to get long URL, """ +
+                                """provide short URL without any other arguments.
+                                """),
+                        )
+    parser.add_argument('--generate', action='store_true', default=False,
+                        help="""If argument presented, URL param will be defined as long url and it wil be shorten."""
+                        )
+    parser.add_argument('--short_url', action='store', default=None,
+                        help="""Use this argument and cpecify short URL, if you want to use custom short_url."""
+                        )
+
+    return parser                
+
 
 def get_long_url(short):
     if short_url_exist(short):
