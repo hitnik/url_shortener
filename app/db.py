c09f30588246a1f94c5f91455f260700328f254e
hitnik <hitnik@gmail.com>
2021-08-18 11:31:24 +0300

    add insert short_url  function

--- app/db.py
+++ app/db.py
@@ -1,9 +1,11 @@
 import sqlite3
 import os
+from contextlib import contextmanager
 
 BASEDIR = os.path.dirname(__file__)
 
 DB_PATH = os.path.join(BASEDIR, 'db.sqlite3')
+
 
 def init_db(db_path):
     """init sqlite database, create sqlite file if needed
@@ -18,6 +20,7 @@ def init_db(db_path):
         db.executescript(file.read().decode('utf8'))
     return db
 
+
 def get_db():
     """returns sqlite3 connection
 
@@ -26,6 +29,7 @@ def get_db():
     """
     db = sqlite3.connect(DB_PATH, detect_types=sqlite3.PARSE_DECLTYPES)
     return db
+
 
 def close_db(db):
     """ close sqlite3.Connection
@@ -36,32 +40,88 @@ def close_db(db):
     if db is not None:
         db.close()
 
-def long_url_exist(url, db):
+
+@contextmanager
+def db_manager():
+    db = get_db()
+    try:
+        yield db
+    finally:
+        close_db(db)
+
+
+@contextmanager
+def cur_manager(db):
+    cur = db.cursor()
+    try:
+        yield cur
+    finally:
+        cur.close()
+
+
+def long_url_exist(url):
     """ checks if row with long_rurl = url exists
 
     Args:
         url (str): url to check
-        db (sqlite3.Connection): db connection
 
     Returns:
         [bool]: returns True when exists
     """
-    cur = db.cursor()
-    exist = cur.execute(f"SELECT EXISTS(SELECT * FROM long_urls WHERE long_url = '{url}')"
-        ).fetchone()
+    with db_manager() as db:
+        cur = db.cursor()
+        exist = cur.execute(f"""SELECT EXISTS(SELECT * FROM long_urls
+                                WHERE long_url = '{url}')"""
+                            ).fetchone()
     if exist[0] > 0:
         return True
     return False
 
 
 def insert_long_url(url):
-    pass
+    """ insert long_url to long_urls table
 
-def insert_short_url(url, long_url):
-    pass
+    Args:
+        url (str): url to insert into table
+
+    Returns:
+        int or None: returns id of row
+        or if IntegrityError raised then returns None
+    """
+    sql = "INSERT INTO long_urls(long_url) VALUES(?);"
+    with db_manager() as db:
+        cur = db.cursor()
+        try:
+            cur.execute(sql, (url,))
+        except sqlite3.IntegrityError:
+            return None
+        db.commit()
+        return cur.lastrowid
+
+
+def insert_short_url(url, long_id):
+    """insert short_url to short_urls table
+    Args:
+        url (str): short url
+        long_id (int): Foreign key id
+
+    Returns:
+        int or None: returns id of row
+        or if IntegrityError raised then returns None
+    """
+    sql = "INSERT INTO short_urls(long_id, short) VALUES(?, ?);"
+    with db_manager() as db:
+        cur = db.cursor()
+        try:
+            -cur.execute(sql, (long_id, url))
+        except sqlite3.IntegrityError:
+            return None
+        db.commit()
+        return cur.lastrowid
 
 def get_short_url(url_long):
     pass
+
 
 if __name__ == '__main__':
     db = init_db(DB_PATH)
@@ -76,4 +136,7 @@ if __name__ == '__main__':
         (2, 2, 'yu.tu');
         """
     db.executescript(script)
-    print(long_url_exist('https://www.google.com/', db))
+    close_db(db)
+    long_id = insert_long_url('https://www.tut.by')
+    print(insert_short_url('http://ex.com/asd', long_id))
+    print(insert_short_url('http://ex.com/asd', long_id))
