d7bd0432361491046865595b5c4428eb200100d2
hitnik <hitnik@gmail.com>
2021-08-18 16:34:20 +0300

    implement  app args

---
+++ app/main.py
@@ -0,0 +1,29 @@
+import argparse
+import textwrap
+
+def parser():
+    parser = argparse.ArgumentParser(description="URL shortener app.", )
+    parser.add_argument('url', metavar='URL', type=str,
+                        help=textwrap.dedent("""
+                            Provide URL here.
+                            If you want to shorten URL, provide it with [--generate] argument.
+                            If you want to get long URL, provide short URL without any other arguments.
+                            """),
+                        )
+    parser.add_argument('--generate', action='store_true', default=False,
+                            help="If argument presented, URL param will be defined as long url and it wil be shorten"
+                        )
+    parser.add_argument('--short_url', action='store', default=None,
+                            help="Use this argument and cpecify short URL, if you want to use custom short_url"
+                        )
+    
+    return parser
+
+if __name__ == '__main__':
+    parser = parser()
+    args = parser.parse_args()
+    print(args)
+    
+
+
+
