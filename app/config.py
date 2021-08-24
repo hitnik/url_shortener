6b1845b7796d7b3dc33df411086b93991910fa38
Aliaksandr Nikitsin <hitnik@gmail.com>
2021-08-24 21:37:40 +0300

    change db peth get

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 BASEDIR = os.path.dirname(__file__)
-DB_PATH = os.path.join(BASEDIR, 'db.sqlite3')
+DB_PATH = os.environ.get('DB_PATH', os.path.join(BASEDIR, 'db.sqlite3'))
 SCHEME = os.environ.get("SCHEME", "http")
 NETLOC = os.environ.get("NETLOC", "localhost")
