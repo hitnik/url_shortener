28dfe2c17df3151229e2f98afc6a7408b33c9383
hitnik <hitnik@gmail.com>
2021-08-27 16:50:54 +0300

    define models

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from app import db
+
+
+class LongUrls(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    long_url = db.Column(db.String, unique=True, index=True)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    
+    def __repr__(self):
+        return f'{self.long_url}'
+
+
+class ShortUrls(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    short = db.Column(db.String(16), unique=True, index=True)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    long_id = db.Column(db.Integer, db.ForeignKey('long_urls.id'))
+    long = db.relationship('LongUrls',
+        backref=db.backref('shorts', lazy=True))
+
+    def __repr__(self):
+        return f'{self.short_url}'
