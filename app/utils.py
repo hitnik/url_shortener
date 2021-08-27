651fe1f15af74c5494f5f01f8a06be87185b6a4e
hitnik <hitnik@gmail.com>
2021-08-27 21:43:14 +0300

    implements utils.py with sqlalhemy

--- app/utils.py
+++ app/utils.py
@@ -23,32 +23,6 @@ class URLNotFoundError(Exception):
         self.message = "This short URL does not exist"
 
 
-def parser():
-    parser = argparse.ArgumentParser(
-        description="URL shortener app.",
-        formatter_class=argparse.RawTextHelpFormatter
-    )
-    parser.add_argument('url', metavar='URL', type=str,
-                        help=textwrap.dedent("""
-                                Provide URL here.\r
-                                If you want to shorten URL, """ +
-                                             """provide it with [--generate] argument.\r
-                                If you want to get long URL, """ +
-                                             """provide short URL without any other arguments.
-                                """),
-                        )
-    parser.add_argument('--generate', action='store_true', default=False,
-                        help="If argument presented, URL param will be " +
-                        "defined as long url and it wil be shorten."
-                        )
-    parser.add_argument('--short_url', action='store', default=None,
-                        help="Use this argument and cpecify short URL, " +
-                        "if you want to use custom short_url."
-                        )
-
-    return parser
-
-
 class Shortener:
 
     def __parse_short_url(self, url):
@@ -66,8 +40,7 @@ class Shortener:
         short_path = cls().__parse_short_url(short)
         if short_url_exist(short_path):
             short_inst = get_short_url(short_path)
-            long_inst = get_long_url_from_db(id=short_inst[1])
-            return long_inst[1]
+            return short_inst.long.long_url
         else:
             raise URLNotFoundError
 
@@ -79,8 +52,8 @@ class Shortener:
         """
         if long_url_exist(long):
             long_inst = get_long_url_from_db(url=long)
-            short_inst = get_short_url_by_long(long_inst[0])
-            return cls.unparse_short_url(short_inst[2])
+            short_inst = get_short_url_by_long(long_inst)
+            return cls.unparse_short_url(short_inst.short)
         else:
             uuid = shortuuid.uuid(name=long)[:7]
             short_url = urlunsplit((SCHEME, NETLOC, uuid, '', ''))
@@ -92,7 +65,7 @@ class Shortener:
     def save_url(short, long):
         if not short_url_exist(short):
             if long_url_exist(long):
-                long_inst = get_long_url_from_db(url=long)
+                long_inst = get_long_url_from_db(long)
                 insert_short_url(short, long_inst)
             else:
                 long_inst = insert_long_url(long)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,27 +1,24 @@
 from urllib.parse import urlunsplit
-
 import pytest
+
 from app.config import NETLOC, SCHEME
 from app.utils import Shortener, URLExistsError, URLNotFoundError
 
-from tests.test_db import manager_mock, script
 
 def build_url(path):
     return urlunsplit((SCHEME, NETLOC, path, '', ''))
 
-def test_get_long_url(db_mock):
-    db_mock.executescript(script)
-    with manager_mock(db_mock, 'db_utils.get_db', 'db_utils.db_manager'):
+def test_get_long_url(app):
+    with app.app_context():
         with pytest.raises(URLNotFoundError):
             Shortener.get_long_url('raise')
-        inst = Shortener.get_long_url('goo.gl')
-        assert isinstance(inst, str)
-        assert inst == 'https://www.google.com/'
+        short = Shortener.get_long_url('goo.gl')
+        assert isinstance(short, str)
+        assert short == 'https://www.google.com/'
 
 
-def test_get_short_url(db_mock):
-    db_mock.executescript(script)
-    with manager_mock(db_mock, 'db_utils.get_db', 'db_utils.db_manager'):
+def test_get_short_url(app):
+    with app.app_context():
         inst = Shortener.gen_short_url('https://www.google.com/')
         assert isinstance(inst, str)
         assert inst == urlunsplit((SCHEME, NETLOC, 'goo.gl', '', ''))
@@ -29,9 +26,8 @@ def test_get_short_url(db_mock):
         assert isinstance(inst, str)
 
 
-def test_save_url(db_mock):
-    db_mock.executescript(script)
-    with manager_mock(db_mock, 'db_utils.get_db', 'db_utils.db_manager'):
+def test_save_url(app):
+    with app.app_context():
         with pytest.raises(URLExistsError):
             Shortener.save_url('goo.gl', 'https://www.google.com/')
         short = Shortener.save_url('onl', 'http://www.onliner.by')
